4f10bafaad5a29e73f2b35bcb5dea61f871ae4ea
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-12-28 17:53:02 +0330

    config backend and done

--- Back-End/register.php
+++ Back-End/register.php
@@ -12,7 +12,7 @@ $repassword = $_POST["repassword"];
 // checking password for login 
 if($password === $repassword){
     $password = md5($password);
-    $query = "insert into tbl_users (name, email, password) values ('?','?','?')";
+    $query = "insert into tbl_users (name, email, password) values (?,?,?)";
    try{
     $statement = $db->prepare($query);
     // binding 
