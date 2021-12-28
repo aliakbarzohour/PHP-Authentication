f58691a8484f64cfac7359096fc2029869630674
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-12-28 18:28:51 +0330

    conecting to external my sql

--- Back-End/config/config.php
+++ Back-End/config/config.php
@@ -1,4 +1,4 @@
 <?php
-$db = new mysqli("5000:500", "root", "", "authentication");
+$db = new mysqli("507c907d-1d44-452a-9db7-6cf0e304eb4b.hsvc.ir:32597", "root", "BjE8PnJXTvm4S2uSwgXYjNtwMnm6mv5j", "authentication");
 
 $db->set_charset("utf8");
