e530b7968d59704bc57894099d5e399693dd24ba
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-12-28 17:51:17 +0330

    make a php file for register page

--- Back-End/config/config.php
+++ Back-End/config/config.php
@@ -2,15 +2,3 @@
 $db = new mysqli("localhost", "root", "", "authentication");
 
 $db->set_charset("utf8");
-
-$name = $_POST["name"];
-$email = $_POST["email"];
-$password = $_POST["password"];
-$repassword = $_POST["repassword"];
-
-if($password === $repassword){
-    $password = md5($password);
-    echo $password;
-}else{
-    echo 'passwod not match';
-}
--- Back-End/register.php
+++ Back-End/register.php
@@ -1,3 +1,30 @@
 <?php
 
 require 'config/config.php';
+// define User name 
+$name = $_POST["name"];
+// define Email 
+$email = $_POST["email"];
+// define password 
+$password = $_POST["password"];
+// define repassword 
+$repassword = $_POST["repassword"];
+// checking password for login 
+if($password === $repassword){
+    $password = md5($password);
+    $query = "insert into tbl_users (name, email, password) values ('?','?','?')";
+   try{
+    $statement = $db->prepare($query);
+    // binding 
+    $statement->bind_param("sss", $name, $email, $password);
+    // executing data 
+    $statement->execute();
+    // and closing database 
+    $statement->close();
+   }catch (Exception $e){
+    echo $e->getMessage();
+   }
+
+}else{
+    echo 'passwod not match';
+}
