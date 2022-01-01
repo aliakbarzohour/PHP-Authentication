6796d881c5757a0c37b6b5c1bd2d6baf25e4d683
AliAkbar_Zohour <71524940+aliakbarzohour@users.noreply.github.com>
2022-01-01 11:30:25 +0330

    Update config.php

--- Back-End/config/config.php
+++ Back-End/config/config.php
@@ -1,4 +1,7 @@
 <?php
+// this sql for hamravesh paas
+// $db = new mysqli("507c907d-1d44-452a-9db7-6cf0e304eb4b.hsvc.ir:32597", "root", "BjE8PnJXTvm4S2uSwgXYjNtwMnm6mv5j", "authentication");
+
 $db = new mysqli("507c907d-1d44-452a-9db7-6cf0e304eb4b.hsvc.ir:32597", "root", "BjE8PnJXTvm4S2uSwgXYjNtwMnm6mv5j", "authentication");
 
 $db->set_charset("utf8");
