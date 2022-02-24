c151fe69ad47bff84c5432ebe1f0cc0a8f21d917
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-02-24 23:25:16 +0330

    editing login page and register page in php files and make a new html file for headering

--- Back-End/login.php
+++ Back-End/login.php
@@ -1,27 +1,28 @@
 <?php
-
 require 'config/config.php';
-// define User name 
-$name = $_POST["name"];
+
 // define Email 
 $email = $_POST["email"];
 // define password 
 $password = $_POST["password"];
-// define repassword 
-$repassword = $_POST["repassword"];
 // checking password for login 
-$password = md5($password);
+$password = sha1($password);
 $query = "select id from tbl_users where email = ? and password = ?";
 try{
  $statement = $db->prepare($query);
  // binding 
- $statement->bind_param("ss", $name, $email, $password);
+ $statement->bind_param("ss", $email, $password);
  // executing data 
  $statement->execute();
+
+ 
  // couting users in database 
  $result = $statement->get_result();
     if($result->num_rows === 1){
-        echo 'Verrry goood .'
+        header('Location: ./done.html');
+        // echo 'OK You LOGED IN ✅';
+    }else{
+        header('Location: ../Front-End/login-page/index.html');
     }
 }catch (Exception $e){
  echo $e->getMessage();
--- Back-End/register.php
+++ Back-End/register.php
@@ -10,21 +10,24 @@ $password = $_POST["password"];
 // define repassword 
 $repassword = $_POST["repassword"];
 // checking password for login 
-if($password === $repassword){
+if ($password === $repassword) {
     $password = sha1($password);
     $query = "insert into tbl_users (name, email, password) values (?,?,?)";
-   try{
-    $statement = $db->prepare($query);
-    // binding 
-    $statement->bind_param("sss", $name, $email, $password);
-    // executing data 
-    $statement->execute();
-    // and closing database 
-    $statement->close();
-   }catch (Exception $e){
-    echo $e->getMessage();
-   }
-   echo 'ok ✅ . your user name and password and email is define on database 👌🏼 .';
-}else{
+    try {
+        $statement = $db->prepare($query);
+        // binding 
+        $statement->bind_param("sss", $name, $email, $password);
+        // executing data 
+        $statement->execute();
+        // and closing database 
+        $statement->close();
+        $db->close();
+        // routing in done page 
+        header('loaction: done.html');
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+    // echo 'ok ✅ . your user name and password and email is define on database 👌🏼 .';
+} else {
     echo 'passwod not match 🚫 . try agin';
 }
